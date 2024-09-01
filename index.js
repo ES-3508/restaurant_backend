7da1259317ae7ddf54383fdc849f05b50dee85f9
Eranda Sanjaya <76166067+ES-3508@users.noreply.github.com>
2024-09-01 23:34:12 +0530

    test

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
-import userRouter from "./routes/userRoute.js"
-import 'dotenv/config'
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
 
-// app config
-const app = express()
-const port = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
-//middleware
-app.use(express.json())
-app.use(cors())
-
-//db connection
-connectDB();
-
-//api endpoints
-app.use("/api/food",foodRouter)
-// app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
-
-app.get("/",(req,res)=>{
-    res.send("API WORKING")
-})
-
-app.listen(port,()=>{
-  console.log(`Server Started on http:/localhost:${port}`)
-})
-
-//mongodb+srv://nawodya:nawodya1234@cluster0.f8lkd.mongodb.net/?
+export default app;
