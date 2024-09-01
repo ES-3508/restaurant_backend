688fbff8fd80720b04cb1b37d7e6fd745d4492aa
Eranda Sanjaya <76166067+ES-3508@users.noreply.github.com>
2024-09-01 23:43:17 +0530

    test

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-import express from 'express';
+const express = require('express');
+const path = require("path");
+require("dotenv").config();
+require("dotenv").config({ path: path.join(__dirname, ".env") });
+const { connectDB } = require('./config/db.js');
+const foodRouter = require('./routes/foodRoute.js');
+const userRouter = require('./routes/userRoute.js');
+const cartRouter = require('./routes/cartRoute.js');
+const orderRouter = require('./routes/orderRoute.js');
+
+const cors = require("cors");
+const helmet = require("helmet");
 
 const app = express();
 app.use(express.json());
@@ -9,9 +20,17 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+//db connection
+connectDB();
+
+//api endpoints
+app.use("/api/food", foodRouter);
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-export default app;
+module.exports = app;
