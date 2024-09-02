e9b1445837b5670af57432947170289787b8ac50
Eranda Sanjaya <76166067+ES-3508@users.noreply.github.com>
2024-09-02 11:22:50 +0530

    fix

--- index.js
+++ index.js
@@ -1,83 +1,37 @@
-// Import necessary modules
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const helmet = require("helmet");
-const bodyParser = require("body-parser");
-require("dotenv").config();
-require("dotenv").config({ path: path.join(__dirname, ".env") });
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
-// Import Routes
-// const foodRouter = require('./routes/foodRoute.js');
-const userRouter = require('./routes/userRoute.js');
-const cartRouter = require('./routes/cartRoute.js');
-const orderRouter = require('./routes/orderRoute.js');
 
-const app = express();
+// app config
+const app = express()
+const port = process.env.PORT || 4000;
 
-// MongoDB connection
-const option = {
-    socketTimeoutMS: 30000,
-};
+//middleware
+app.use(express.json())
+app.use(cors())
 
-const mongoURI = process.env.MONGODB_URI;
-mongoose.connect(mongoURI, option).then(
-  () => {
-    console.log("MongoDB connected!");
-  },
-  (err) => {
-    console.log("Failed to connect to MongoDB", 'mongodb+srv://nawodya:nawodya1234@cluster0.f8lkd.mongodb.net/food-del');
-  }
-);
+//db connection
+connectDB();
 
-// Middleware
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+//api endpoints
+app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
 
-const corsOptions = {
-  origin: [
-    'https://the-toucan-app-react.vercel.app/', 
-    'https://toucan.supersconto24.com/api/v1/auth/me', 
-    'https://toucan.supersconto24.com'
-  ],
-  credentials: true, 
-  optionSuccessStatus: 200
-};
-app.use(cors(corsOptions));
+app.get("/",(req,res)=>{
+    res.send("API WORKING")
+})
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(port,()=>{
+  console.log(`Server Started on http:/localhost:${port}`)
+})
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.setHeader('Cache-Control', 'no-cache, no-store');
-  next();
-});
-
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-// app.use("/api/food", foodRouter);
-app.use("/api/user", userRouter);
-app.use("/api/cart", cartRouter);
-app.use("/api/order", orderRouter);
-
-// Set port and listen for requests
-const APP_PORT = process.env.APP_PORT || 5000;
-app.use(require('express-status-monitor')());
-app.disable('x-powered-by');
-
-app.listen(APP_PORT, () => {
-  console.log(`Server is running on http://localhost:${APP_PORT}`);
-});
-
-module.exports = app;
+//mongodb+srv://nawodya:nawodya1234@cluster0.f8lkd.mongodb.net/?
